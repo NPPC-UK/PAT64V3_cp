635b339dd0b6648b34f7a63ed6ca93c9f9f12038
Maximilian Friedersdorff <max@friedersdorff.com>
2018-10-26 15:59:01 +0100

    Document the OnCountPixels function

--- analysis_utils.h
+++ analysis_utils.h
@@ -2,7 +2,13 @@
 #define ANAL_UTILS
 #include <opencv2/opencv.hpp>
 
-
+/**
+ * Count the number of pixels with intensity <=210.
+ *
+ * Convert the input image to grayscale first if
+ * necessary.  Count only pixels in the central 70% of columns 
+ * and between rows pottop and planttop.
+ */
 int OnCountPixels(cv::Mat img, int pottop, int planttop);
 cv::Mat RestoreImgFromTemp(cv::Mat temp, cv::Mat source);
 #endif
